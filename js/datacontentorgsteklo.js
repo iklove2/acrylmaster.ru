3aac22f423c0f60c65e3b66eeba471d026eac232
Ivan Kalugin <42765419+iklove2@users.noreply.github.com>
2024-05-11 09:35:05 +0300

    fix add orgstekloglass

---
+++ js/datacontentorgsteklo.js
@@ -0,0 +1,62 @@
+const dataJson = `[
+  { 
+    "img" : "img/page27/kc-1.webp",
+    "title" : "kc-1",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 52
+  },
+  { 
+    "img" : "img/page27/kc-2.webp",
+    "title" : "kc-2",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 24
+  },
+  { 
+    "img" : "img/page27/kc-3.webp",
+    "title" : "kc-3",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 35
+  },
+  { 
+    "img" : "img/page27/kc-4.webp",
+    "title" : "kc-4",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 78
+  },
+  { 
+    "img" : "img/page27/kc-5.webp",
+    "title" : "kc-5",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 21
+  },
+  { 
+    "img" : "img/page27/kc-6.webp",
+    "title" : "kc-6",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 76
+  },
+  { 
+    "img" : "img/page27/kc-7.webp",
+    "title" : "kc-7",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 54
+  },  
+  { 
+    "img" : "img/page27/kc-8.webp",
+    "title" : "kc-8",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 21
+  },
+  { 
+    "img" : "img/page27/kc-9.webp",
+    "title" : "kc-9",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 76
+  },
+  { 
+    "img" : "img/page27/kc-10.webp",
+    "title" : "kc-10",
+    "text" : "3D Coffee Keychain Set",
+    "price" : 54
+  }
+]`
